0a9e0417997d7d8e271ab491f82286f85c5ff3b9
Imbeshat <md.imbeshat@gmail.com>
2023-04-11 02:16:11 +0530

    Database connection done with env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import app from "./app.js";
+import config from "./config/index.js";
 
 // Create and run the method to connect to the database. We can use IIFE (Immediately Invoked Function Expression) to run the method.
 
 (async () => {
 	try {
-		await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+		await mongoose.connect(config.MONGODB_URL);
 		console.log("Database Connected!");
 
 		// Error express might throw - to handle that error
@@ -15,9 +16,9 @@ import app from "./app.js";
 		});
 
 		const onListening = () => {
-			console.log("Listening on port 5000");
+			console.log(`Listening on port ${config.PORT}`);
 		};
-		app.listen(5000, onListening);
+		app.listen(config.PORT, onListening);
 	} catch (error) {
 		console.error("ERROR: ", error);
 		throw error;
