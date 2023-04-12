f57e633d749dc09a924a7ceb467c6b956dc43443
Imbeshat <md.imbeshat@gmail.com>
2023-04-12 19:45:38 +0530

    forgot password functionality done

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -5,6 +5,8 @@ dotenv.config();
 const config = {
 	PORT: process.env.PORT || 5000,
 	MONGODB_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/ecommerce",
+	JWT_SECRET: process.env.JWT_SECRET || "yorsecret",
+	JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
 };
 
 export default config;
