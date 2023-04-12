d0fa45bc6af2e75dbb0be2a34d4a80ca6a28f64e
Imbeshat <md.imbeshat@gmail.com>
2023-04-12 22:52:10 +0530

    product schema almost done

---
+++ backend/src/models/product.schema.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Please enter product name"],
+			trim: true,
+			maxLength: [100, "Product name cannot exceed 100 characters"],
+		},
+		price: {
+			type: Number,
+			required: [true, "Please enter product price"],
+			maxLength: [5, "Product name cannot exceed 5 characters"],
+		},
+		description: {
+			type: String,
+			required: [true, "Please enter product description"],
+		},
+		photos: [
+			{
+				secure_url: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+		stock: {
+			type: Number,
+			default: 0,
+		},
+		sold: {
+			type: Number,
+			default: 0,
+		},
+		collectionId: {
+			ref: "Collection",
+		},
+	},
+	{ timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
