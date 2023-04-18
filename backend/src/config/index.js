6bfa6e0071ea4fd889e3dc482f12105e15be4730
Imbeshat <md.imbeshat@gmail.com>
2023-04-18 12:32:05 +0530

    aws s3 image upload

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -7,6 +7,10 @@ const config = {
 	MONGODB_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/ecommerce",
 	JWT_SECRET: process.env.JWT_SECRET || "yorsecret",
 	JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+	S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+	S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+	S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+	S3_REGION: process.env.S3_REGION,
 };
 
 export default config;
