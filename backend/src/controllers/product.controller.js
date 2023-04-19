ccb0922c0983d24e445d48498d2308e92d9a63d4
Imbeshat <md.imbeshat@gmail.com>
2023-04-19 22:35:09 +0530

    photo upload and delete from aws

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -5,6 +5,7 @@ import formidable from "formidable";
 import { s3FileUpload, s3FileDelete } from "../service/imageUpload.js";
 import Mongoose from "mongoose";
 import config from "../config/index.js";
+import fs from "fs";
 
 /**********************************************************
  * @ADD_PRODUCT
@@ -27,5 +28,130 @@ export const addProduct = asyncHandler(async (req, res) => {
 		// Create a product id for the product and save it in the database and extract the details
 		let productId = new Mongoose.Types.ObjectId().toHexString();
 		console.log(fields, files);
+
+		// if any fiels is empty throw an error
+		if (!fields.name || !fields.description || !fields.price || !fields.collectionId) {
+			throw new CustomError("All fields are required", 400);
+		}
+
+		// resolve all the promises which we receive while uploading the image to the s3 bucket
+		let imgArrayResponse = await Promise.all(
+			Object.keys(files).map(async (file, index) => {
+				const element = files[fileKey];
+				console.log(element);
+				const data = fs.readFileSync(element.filepath);
+
+				// upload the image to the s3 bucket
+				const upload = await s3FileUpload({
+					bucketName: config.S3_BUCKET_NAME,
+					key: `products/${productId}/photo_${index + 1}.png`,
+					body: data,
+					contentType: element.mimetype,
+				});
+				console.log(upload);
+				return {
+					secure_url: upload.Location,
+				};
+			})
+		);
+
+		// store in another variable
+		let imgArray = imgArrayResponse;
+
+		// send it to the database
+		const product = await Product.create({
+			_id: productId,
+			photos: imgArray,
+			...fields,
+		});
+
+		// send the response
+		if (!product) {
+			throw new CustomError("Product failed to be created", 400);
+		}
+		res.status(200).json({
+			success: true,
+			product,
+		});
+	});
+});
+
+// Get all products
+export const getAllProducts = asyncHandler(async (req, res) => {
+	const products = await Product.find({});
+
+	if (!products) {
+		throw new CustomError("No products found", 404);
+	}
+
+	res.status(200).json({
+		success: true,
+		products,
+	});
+});
+
+// Get a single product
+export const getSingleProduct = asyncHandler(async (req, res) => {
+	const { id: productId } = req.params;
+	const product = await Product.findById(productId);
+
+	if (!product) {
+		throw new CustomError("No product found", 404);
+	}
+
+	res.status(200).json({
+		success: true,
+		product,
+	});
+});
+
+// Get all products by collection id
+export const getProductByCollectionId = asyncHandler(async (req, res) => {
+	const { id: collectionId } = req.params;
+	const products = await Product.find({ collectionId });
+
+	if (!products) {
+		throw new CustomError("No products found", 404);
+	}
+
+	res.status(200).json({
+		success: true,
+		products,
+	});
+});
+
+// Delete a product
+export const deleteProduct = asyncHandler(async (req, res) => {
+	const { id: productId } = req.params;
+
+	const product = await Product.findById(productId);
+
+	if (!product) {
+		throw new CustomError("No product found", 404);
+	}
+
+	// delete all the images and product from the s3 bucket
+
+	// Steps to delete photos:
+	// 1. Resolve the promise
+	// 2. loop through the photos array
+	// 3. get the key of the image
+	// 4. delete the image from the s3 bucket
+
+	const deletePhotos = Promise.all(
+		product.photos.map(async (elem, index) => {
+			await s3FileDelete({
+				bucketName: config.S3_BUCKET_NAME,
+				key: `products/${product._id.toString()}/photo_${index + 1}.png`,
+			});
+		})
+	);
+
+	await deletePhotos;
+	await product.remove();
+
+	res.status(200).json({
+		success: true,
+		message: "Product deleted successfully",
 	});
 });
